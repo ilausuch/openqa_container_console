3c54ffdb4c597cf21896aabc41f59f5993964568
Ivan Lausuch <ilausuch@suse.com>
2021-06-15 16:13:32 +0200

    Add params check to the scripts

--- clone-job.sh
+++ clone-job.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # clone-job.sh <HOST> <JOB ID> <VARIABLE> 
 
+ if [[ $# -lt 2 ]]; then
+   echo "./clone-job.sh <HOST> <PR> <Old job> <variable>"
+   echo "e.g. ./clone-job.sh openqa.opensuse.org https://openqa.opensuse.org/tests/1788382"
+   exit 1
+fi
+
+
 docker-compose exec console /usr/share/openqa/script/clone_job.pl --within-instance $1 $2 $3
--- openqa-clone-custom-git.sh
+++ openqa-clone-custom-git.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-# openqa-clone-custom-git-opensuse.sh <HOST> <PR> <Old job> <variable>
+if [[ $# -lt 3 ]]; then
+  echo "./openqa-clone-custom-git.sh <HOST> <PR> <Old job> <variable>"
+  echo "e.g. ./openqa-clone-custom.git.sh openqa.opensuse.org https://github.com/os-autoinst/os-autoinst-distri-openQA/pull/70 https://openqa.opensuse.org/tests/1788382"
+  exit 1
+fi
 
 docker-compose exec console /usr/share/openqa/script/openqa-clone-custom-git-refspec $2 $3 $4 --clone-job-args="--host $1"
